899548c5ea2448855d8a8b4dd46748c709db2931
juno <justjjo@koreatech.ac.kr>
2022-06-22 09:59:18 +0900

    새로운 api

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pythonDIR import naver, articleID, commentAPI, alertURLback
 alertWord = ["m.site.naver.com", "bit.ly", "open.kakao.com"]
 now = datetime.datetime.now()
 
+#https://apis.naver.com/cafe-web/cafe-articleapi/v2/cafes/23370764/articles/ + articleid
+
 #차후 댓글 리스트를 파일 형태로 디스코드 봇으로 출력하기 위해 파일 생성 코드를 추가
 fileMake = open("list.txt", "wt")
 fileMake.close()
@@ -12,6 +14,7 @@ useFile = open("list.txt", "at")
 
 #articleID 크롤링 - 로그인 필요가 없음
 articleid = articleID.articleIDget()
+
 
 #네이버 자동 로그인
 driver = naver.login()
