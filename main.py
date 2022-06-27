5b5af39281dd3fb526245357b1f5ada42c7c6572
juno <justjjo@koreatech.ac.kr>
2022-06-27 17:03:50 +0900

    class로 재제작

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import datetime
-from pythonDIR import naver, articleID, jsonanalysis, alertURLback, jsonGet, result
+from pythonDIR import naver, articleID, jsonanalysis, alertURLback, jsonGet, result, jsonanalyse
 
 now = datetime.datetime.now()
 
-#articleID 크롤링 - 로그인 필요가 없음
+# articleID 크롤링 - 로그인 필요가 없음
 articleid = articleID.articleIDget()
 
-#네이버 자동 로그인
+# 네이버 자동 로그인
 driver = naver.login()
 
-# 2. json에서 원하는 요소를 찾아서 변수에 저장하기 + content 분석
-alertComment = jsonanalysis.comment(json)
-alertArticle = jsonanalysis.article(json)
+idlist = []
+for i in articleid:
+    cube = jsonanalyse.check(driver, i)
+    n = cube.articlecheck()
+    m = cube.commentcheck()
+    if not n == None:
+        idlist.append(n)
+    if not m == None:
+        idlist.append(m)
 
-result.res(alertComment, alertArticle, now)
---
+++ pythonDIR/jsonanalyse.py
@@ -0,0 +1,59 @@
+import json
+from pythonDIR import alertword, checkContent
+from bs4 import BeautifulSoup
+
+# jsonanalysis to class
+class check:
+    def __init__(self, driver, articleid):
+        self.driver = driver
+        self.articleid = articleid
+
+        url = 'https://apis.naver.com/cafe-web/cafe-articleapi/v2/cafes/23370764/articles/' + self.articleid
+        self.driver.get(url)
+
+        cr = self.driver.find_element_by_xpath('/html/body/pre').text
+        cr = json.loads(cr)
+
+        self.jsonfile = cr
+
+        self.commenttext = None
+        self.articletext = None
+        self.word = alertword.word()
+
+    def commentcheck(self):
+        word = self.word
+        a = self.jsonfile['result']['comments']['items']
+        self.commenttext = a
+
+        for i in a:
+            content = i['content']
+            n = checkContent.checkComment(content, word)
+            if n == 1:
+                return self.articleid
+            else:
+                return None
+
+
+    def articlecheck(self):
+        word = self.word
+        js = self.jsonfile
+
+        a = js['result']['article']['contentHtml']
+
+        soup = BeautifulSoup(a, 'html.parser')
+        m = soup.get_text()
+        m = m.replace('\n', '')
+        m = m.split('.')
+        for x in m:
+            x = x.split(' ')
+            n = checkContent.checkArticle(x, word)
+            if n == 1:
+                return self.articleid
+            else:
+                return None
+
+    def comment(self):
+        return self.commenttext
+
+    def article(self):
+        return self.articletext
