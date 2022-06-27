046243c15f66ba22ae127dba909098e91be0e83f
juno <justjjo@koreatech.ac.kr>
2022-06-27 16:26:17 +0900

    금칙어 별도 파일로 분리

---
+++ pythonDIR/alertword.py
@@ -0,0 +1,2 @@
+def word():
+    return ["m.site.naver.com", "bit.ly", "open.kakao.com"]
--- pythonDIR/jsonanalysis.py
+++ pythonDIR/jsonanalysis.py
@@ -1,27 +1,25 @@
-from pythonDIR import checkContent
+from pythonDIR import checkContent, alertword
 from bs4 import BeautifulSoup
 
-
-# 알람 설정할 파일
-alertWord = ["m.site.naver.com", "bit.ly", "open.kakao.com"]
-
-
+# 클래스 형태로 제작 하는 것을 고민
+# 현재 상태는 분석을 위해 여러개의 파이썬 파일에서 여러개의 함수를 이용했으나, 클래스를 이용해 되도록 하나로 모으는 것을 고민
 def comment(json):
-    noti =[]
+    word = alertword.word()
+    result = []
     for k in range(len(json)):
         a = json[k]['result']['comments']['items']
         for i in a:
             content = i['content']
-            n = checkContent.checkComment(content, alertWord)
+            n = checkContent.checkComment(content, word)
             if n == 1:
-                noti.append(json[k]['result']['articleId'])
+                result.append(json[k]['result']['articleId'])
 
-    return noti
+    return result
 
 
 def article(json):
+    word = alertword.word()
     result = []
-
     for i in range(0, 10):
         js = json[i]
 
@@ -33,7 +31,7 @@ def article(json):
         m = m.split('.')
         for x in m:
             x = x.split(' ')
-            n = checkContent.checkArticle(x, alertWord)
+            n = checkContent.checkArticle(x, word)
             if n == 1:
                 result.append(js['result']['articleId'])
 
