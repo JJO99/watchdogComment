afa4c6bcd39766ab7721ee5c449342bc2337d932
juno <justjjo@koreatech.ac.kr>
2022-07-12 16:15:23 +0900

    코드 일부 수정

--- discordbot.py
+++ discordbot.py
@@ -18,13 +18,13 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('봇 로그인 성공')
 
-    @tasks.loop(seconds=60) # task runs every 60 seconds
+    @tasks.loop(seconds=60)  # task runs every 60 seconds
     async def my_background_task(self):
-        channel = self.get_channel(personalInfo.chanid()) # channel ID goes here
+        channel = self.get_channel(personalInfo.chanid())  # channel ID goes here
 
         color = discord.Color.from_rgb(31, 132, 255)
 
-        embed1 = discord.Embed(title="감시봇",colour=color)
+        embed1 = discord.Embed(title="감시봇", colour=color)
         embed1.set_author(name="DEVELOID BOT(ALPHA)")
         embed1.add_field(name="**확인 하는 중**", value="약 2초가량 소요됩니다.")
         await channel.send(embed=embed1, delete_after=3)
@@ -32,16 +32,17 @@ class MyClient(discord.Client):
 
         embed2 = discord.Embed(title="감시봇", colour=color)
         embed2.set_author(name="DEVELOID BOT(ALPHA)")
-        articleid = articleID.articleIDget()
+        articleid = articleID.recent_article_id_get()
 
         urllist = []
         for x in articleid:
             temp = analysemain.Analyse(driver, x, word)
             get = temp.one_total_check()
+            urllist.append(get)
         urllist = list(set(urllist))
 
         time = "기준시간: " + self.now
-        if not urllist is []:
+        if not urllist[0] is None:
             url = "everyone 이상 게시글: " + str(urllist)
             embed2.add_field(name="**확인 결과**", value=url, inline=False)
 
@@ -55,7 +56,8 @@ class MyClient(discord.Client):
 
     @my_background_task.before_loop
     async def before_my_task(self):
-        await self.wait_until_ready() # wait until the bot logs in
+        await self.wait_until_ready()  # wait until the bot logs in
+
 
 client = MyClient()
 client.run(personalInfo.token())
