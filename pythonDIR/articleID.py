99f8dbe49eb72b9a5fdb97d5912cadf2bc0b0bc2
juno <justjjo@koreatech.ac.kr>
2022-07-14 09:15:47 +0900

    주석 수정, 불필요 import 구문 삭제

--- pythonDIR/articleID.py
+++ pythonDIR/articleID.py
@@ -1,12 +1,11 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 
 
 def recent_article_id_get ():
+    # 최근 댓글이 달린 게시글 10개에 대해서 articleid를 파싱합니다.
     articleid = []
     url = 'https://cafe.naver.com/develoid'
-    # 최신 댓글이 달린 게시글을 긁어오기
     response = requests.get(url)
 
     html = response.text
@@ -37,7 +36,7 @@ def recent_article_id_get ():
 
 
 def all_article_id_get():
-    # 최근 게시글 40개의 "게시글 내용" 분석만을 위함
+    # 최근 올라온 게시글 40개에 대해서 articleid를 파싱합니다.
     article_id = []
     url = 'https://cafe.naver.com/ArticleList.nhn?search.clubid=23370764&search.boardtype=L&search.menuid=&search.marketBoardTab=D&search.specialmenutype=&userDisplay=40'
     r = requests.get(url)
